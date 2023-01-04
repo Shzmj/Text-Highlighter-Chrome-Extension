9d89304dfa68e3bdd6768f39fdcf9640b4501a4e
Shzmj <109339363+Shzmj@users.noreply.github.com>
2023-01-04 12:30:01 +1100

    Fixed highlighter issue. Added new highlighter colours. Added CSS to popup

--- src/content.js
+++ src/content.js
@@ -3,25 +3,46 @@ let clicked = false;
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     // checking if popup button has been clicked
-    if (request.task === "yellowHighlighter") {
-        // getting the users selection
-        let selection = window.getSelection();
-
-        // creating a new text node to replace the original users selection
-        let highlightedText = document.createElement("span");
-        highlightedText.style['background-color'] = '#FFFF00';
-        highlightedText.style.color = "black";
-
-        // checking that characters/text have been selected
-        if (selection.rangeCount) {
-            let range = selection.getRangeAt(0).cloneRange();
-            range.surroundContents(highlightedText);
-            selection.removeAllRanges();
-            selection.addRange(range);
-        }
+    if (request.task) {
+        highlight(request.task);
     }
 
     clicked = !clicked;
     const response = { status: "done" };
     sendResponse(response);
 });
+
+function makeEditableAndHighlight(colour) {
+    var range, sel = window.getSelection();
+    if (sel.rangeCount && sel.getRangeAt) {
+        range = sel.getRangeAt(0);
+    }
+    document.designMode = "on";
+    if (range) {
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
+    // Use HiliteColor since some browsers apply BackColor to the whole block
+    if (!document.execCommand("HiliteColor", false, colour)) {
+        document.execCommand("BackColor", false, colour);
+    }
+    document.designMode = "off";
+}
+
+function highlight(colour) {
+    var range, sel;
+    if (window.getSelection) {
+        // IE9 and non-IE
+        try {
+            if (!document.execCommand("BackColor", false, colour)) {
+                makeEditableAndHighlight(colour);
+            }
+        } catch (err) {
+            makeEditableAndHighlight(colour)
+        }
+    } else if (document.selection && document.selection.createRange) {
+        // IE <= 8 case
+        range = document.selection.createRange();
+        range.execCommand("BackColor", false, colour);
+    }
+}
--- src/popup.js
+++ src/popup.js
@@ -1,13 +1,77 @@
-const btn = document.getElementById("clickBtn");
+const btnYellow = document.getElementById("clickBtnYellow");
+const btnPink = document.getElementById("clickBtnPink");
+const btnGreen = document.getElementById("clickBtnGreen");
+const btnBlue = document.getElementById("clickBtnBlue");
+const btnClear = document.getElementById("clickBtnClear");
 
-btn.addEventListener('click', () => {
+btnYellow.addEventListener('click', () => {
     let queryOptions = { active: true, currentWindow: true };
     chrome.tabs.query(queryOptions, (tabs) => {
         console.log(tabs[0].id);
         chrome.tabs.sendMessage(
             tabs[0].id,
             {
-                task: "yellowHighlighter"
+                task: "yellow"
+            },
+            function (response) {
+                console.log(response.status);
+            });
+    })
+})
+
+btnPink.addEventListener('click', () => {
+    let queryOptions = { active: true, currentWindow: true };
+    chrome.tabs.query(queryOptions, (tabs) => {
+        console.log(tabs[0].id);
+        chrome.tabs.sendMessage(
+            tabs[0].id,
+            {
+                task: "#FA3BF0"
+            },
+            function (response) {
+                console.log(response.status);
+            });
+    })
+})
+
+btnGreen.addEventListener('click', () => {
+    let queryOptions = { active: true, currentWindow: true };
+    chrome.tabs.query(queryOptions, (tabs) => {
+        console.log(tabs[0].id);
+        chrome.tabs.sendMessage(
+            tabs[0].id,
+            {
+                task: "#5DE23C"
+            },
+            function (response) {
+                console.log(response.status);
+            });
+    })
+})
+
+btnBlue.addEventListener('click', () => {
+    let queryOptions = { active: true, currentWindow: true };
+    chrome.tabs.query(queryOptions, (tabs) => {
+        console.log(tabs[0].id);
+        chrome.tabs.sendMessage(
+            tabs[0].id,
+            {
+                task: "#30C5FF"
+            },
+            function (response) {
+                console.log(response.status);
+            });
+    })
+})
+
+btnClear.addEventListener('click', () => {
+    let queryOptions = { active: true, currentWindow: true };
+    chrome.tabs.query(queryOptions, (tabs) => {
+        console.log(tabs[0].id);
+        chrome.tabs.sendMessage(
+            tabs[0].id,
+            {
+                task: "transparent"
             },
             function (response) {
                 console.log(response.status);
