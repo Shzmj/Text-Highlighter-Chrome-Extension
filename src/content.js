f31c2b5a77a0ba7a13820df664e634e2a958e2cc
Shzmj <109339363+Shzmj@users.noreply.github.com>
2023-01-06 16:10:58 +1100

    Final touches

--- src/content.js
+++ src/content.js
@@ -1,24 +1,54 @@
 console.log("Chrome extension ready to go!");
-let clicked = false;
 
 chrome.runtime.onMessage.addListener((request) => {
     // checking if popup button has been clicked
     if (request.task) {
         highlight(request.task);
     }
-
-    clicked = !clicked;
 });
 
+/*
+* The below functions were sourced from https://stackoverflow.com/a/3224513
+* Author: Tim Down
+* How they work:
+*   - Essentially the below highlight() and makeEditableAndHighlight() functions 
+*     use the execCommand() which allows us to manipulate any region of the 
+*     document when it is in designMode (including selections that span 
+*     across multiple elements/nodes).
+* 
+* Another potential solution could be:
+
+    // getting the users selection
+    let selection = window.getSelection();
+
+    // creating a new text node to replace the original users selection
+    let highlightedText = document.createElement("span");
+    highlightedText.style['background-color'] = '#FFFF00';
+    highlightedText.style.color = "black";
+
+    // checking that characters/text have been selected
+    if (selection.rangeCount) {
+        let range = selection.getRangeAt(0).cloneRange();
+        range.surroundContents(highlightedText);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+
+* However the issue with the code above is that it will only highlight
+* elements within the same node, and if they are not in the same node 
+* it will simply insert an empty span element.
+*/
+
 function makeEditableAndHighlight(colour) {
-    var range, sel = window.getSelection();
-    if (sel.rangeCount && sel.getRangeAt) {
-        range = sel.getRangeAt(0);
+    let range;
+    let selection = window.getSelection();
+    if (selection.rangeCount && selection.getRangeAt) {
+        range = selection.getRangeAt(0);
     }
     document.designMode = "on";
     if (range) {
-        sel.removeAllRanges();
-        sel.addRange(range);
+        selection.removeAllRanges();
+        selection.addRange(range);
     }
     // Use HiliteColor since some browsers apply BackColor to the whole block
     if (!document.execCommand("HiliteColor", false, colour)) {
@@ -28,7 +58,6 @@ function makeEditableAndHighlight(colour) {
 }
 
 function highlight(colour) {
-    var range, sel;
     if (window.getSelection) {
         // IE9 and non-IE
         try {
@@ -40,7 +69,7 @@ function highlight(colour) {
         }
     } else if (document.selection && document.selection.createRange) {
         // IE <= 8 case
-        range = document.selection.createRange();
+        let range = document.selection.createRange();
         range.execCommand("BackColor", false, colour);
     }
 }
